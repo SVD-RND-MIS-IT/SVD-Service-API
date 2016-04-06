ddcd4037837e50e20949211f6b1a3062d0f04a0f
Nuwanthi <bngunawardana@gmail.com>
2016-04-07 01:56:31 +0530

    prefect section registration service done Signed-off-by: Nuwanthi <bngunawardana@gmail.com>

---
+++ svdmisservice/model/prefect_managment/PrefectSectionManagement.php
@@ -0,0 +1,182 @@
+<?php
+require_once '../../model/commen/PassHash.php';
+/**
+ * Class to handle all the prefect section details
+ * This class will have CRUD methods for prefect_section
+ *
+ * @author Bagya
+ *
+ */
+
+class PrefectSectionManagement {
+
+    private $conn;
+
+    function __construct() {
+        require_once '../../model/commen/DbConnect.php';
+        // opening db connection
+        $db = new DbConnect();
+        $this->conn = $db->connect();
+    }
+	
+	
+/*
+ * ------------------------ PREFECT_SECTION TABLE METHODS ------------------------
+ */
+
+    /**
+     * Creating new prefect sections
+     *
+     * @param String $sec_name section name for the system
+	 * @param String $recode_added_by 
+     *
+     * @return database transaction status
+     */
+    public function createPrefectSection($sec_name,$recode_added_by ) {
+
+		
+        $response = array();
+		
+        // First check if section already existed in db
+        if (!$this->isSectionExists($sec_name)) {
+  
+            // insert query
+			 $stmt = $this->conn->prepare("INSERT INTO prefect_section	 (sec_name, recode_added_by) values(?, ?)");
+			 $stmt->bind_param("si", $sec_name, $recode_added_by );
+			 $result = $stmt->execute();
+
+			 $stmt->close();
+
+        } else {
+            // section is not already existed in the db
+            return ALREADY_EXISTED;
+        }
+		
+         
+
+        // Check for successful insertion
+        if ($result) {
+			// section successfully inserted
+            return CREATED_SUCCESSFULLY;
+        } else {
+            // Failed to create section
+            return CREATE_FAILED;
+        }
+        
+		return $response;
+
+    }
+	
+	
+	
+/**
+     * Delete prefect possition
+     *
+     * @param String $tal_name Talant name for the system
+	 * @param String $recode_added_by
+     *
+     * @return database transaction status
+     */
+    public function deletePrefectPossition($pos_name, $recode_added_by) {
+
+		
+        $response = array();
+        // First check if possition already existed in db
+        if ($this->isPossitionExists($pos_name)) {
+           			
+			//
+			$stmt = $this->conn->prepare("UPDATE prefect_possition set status = 3, recode_modified_at = now() , recode_modified_by = ? where pos_name = ? and (status=1 or  status=2)");
+			$stmt->bind_param("is",$recode_added_by, $pos_name);
+			$result = $stmt->execute();
+			
+            $stmt->close();
+
+        } else {
+            // Possition is not already existed in the db
+            return NOT_EXISTED;
+        }
+		
+         
+
+        // Check for successful insertion
+        if ($result) {
+			// possition successfully deleted
+            return DELETE_SUCCESSFULLY;
+        } else {
+            // Failed to delete possition
+            return DELETE_FAILED;
+        }
+        
+		return $response;
+
+    }
+	  
+	/**
+     * Fetching prefect possition by pos_name
+	 *
+     * @param String $pos_name possition name
+	 *
+	 *@return prefect possition object only needed data
+     */
+    public function getPossitionByPossitionName($pos_name) {
+        $stmt = $this->conn->prepare("SELECT pos_name, status, recode_added_at, recode_added_by FROM prefect_possition WHERE pos_name = ? and (status=1 or status=2)");
+        $stmt->bind_param("s", $pos_name);
+        if ($stmt->execute()) {
+            $stmt->bind_result($pos_name,$status, $recode_added_at, $recode_added_by);
+            $stmt->fetch();
+            $possition = array();
+            $possition["pos_name"] = $pos_name;
+            $possition["status"] = $status;
+            $possition["recode_added_at"] = $recode_added_at;
+			$possition["recode_added_by"] = $recode_added_by;
+
+            $stmt->close();
+            return $possition;
+        } else {
+            return NULL;
+        }
+    }
+  
+  
+	/**
+     * Fetching all prefect possitions
+	 *
+     * @return $possition object set of all possitions
+     */
+    public function getAllPossitions() {
+        $stmt = $this->conn->prepare("SELECT * FROM prefect_possition WHERE status = 1 or  status = 2");
+        $stmt->execute();
+        $possitions = $stmt->get_result();
+        $stmt->close();
+        return $possitions;
+    }
+	
+  
+  
+  
+  
+  
+/*
+ * ------------------------ SUPPORTIVE METHODS ------------------------
+ */
+
+	/**
+     * Checking for duplicate sections by sec_name
+     *
+     * @param String $sec_name Possition name to check in db
+     *
+     * @return boolean
+     */
+    private function isSectionExists($sec_name) {
+		$stmt = $this->conn->prepare("SELECT sec_name from prefect_section WHERE (status = 1 or status = 2)  and sec_name = ?  ");
+        $stmt->bind_param("s",$sec_name);
+        $stmt->execute();
+		$stmt->store_result();
+        $num_rows = $stmt->num_rows;
+        $stmt->close();
+        return ($num_rows > 0); //if it has more than zero number of rows; then  it sends true
+    }
+
+}
+
+?>
---
+++ svdmisservice/service/prefect_managment/PrefectSectionService.php
@@ -0,0 +1,240 @@
+<?php
+require_once '../../model/user_management/OperationalUserManagement.php';
+require_once '../../model/prefect_managment/PrefectSectionManagement.php';
+require '../.././config/libs/Slim/Slim.php';
+
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+
+// User id from db - Global Variable
+$currunt_user_id = NULL;
+
+/**
+ * Adding Middle Layer to authenticate every request
+ * Checking if the request has valid api key in the 'Authorization' header
+ */
+function authenticate(\Slim\Route $route) {
+    // Getting request headers
+    $headers = apache_request_headers();
+    $response = array();
+    $app = \Slim\Slim::getInstance();
+
+    // Verifying Authorization Header
+    if (isset($headers['Authorization'])) {
+        $operationalUserManagement = new OperationalUserManagement();
+
+        // get the api key
+        $api_key = $headers['Authorization'];
+        // validating api key
+        if (!$operationalUserManagement->isValidApiKey($api_key)) {
+            // api key is not present in users table
+            $response["error"] = true;
+            $response["message"] = "Access Denied. Invalid Api key";
+            echoRespnse(401, $response);
+            $app->stop();
+        } else {
+            global $currunt_user_id;
+            // get user primary key id
+            $currunt_user_id = $operationalUserManagement->getUserId($api_key);
+        }
+    } else {
+        // api key is missing in header
+        $response["error"] = true;
+        $response["message"] = "Api key is misssing";
+        echoRespnse(400, $response);
+        $app->stop();
+    }
+}
+
+/*
+ * ------------------------ PREFECT_SECTION TABLE METHODS ------------------------
+ */
+ 
+/**
+ * Prefect Section Registration
+	 
+ * method - POST
+ * params - sec_name
+ */
+$app->post('/prefect_section_register',  'authenticate', function() use ($app) {
+	
+            // check for required params
+            verifyRequiredParams(array('sec_name'));
+			
+			global $currunt_user_id;
+
+            $response = array();
+
+            // reading post params
+            $sec_name = $app->request->post('sec_name');
+         
+           
+            $prefectSectionManagement = new PrefectSectionManagement();
+			$res = $prefectSectionManagement->createPrefectSection($sec_name,$currunt_user_id);
+			
+            if ($res == CREATED_SUCCESSFULLY) {
+                $response["error"] = false;
+                $response["message"] = "Prefect section is successfully registered";
+            } else if ($res == CREATE_FAILED) {
+                $response["error"] = true;
+                $response["message"] = "Oops! An error occurred while registereing prefect section";
+            } else if ($res == ALREADY_EXISTED) {
+                $response["error"] = true;
+                $response["message"] = "Sorry, this prefect section already exist";
+            }
+            // echo json response
+            echoRespnse(201, $response);
+        });
+
+
+
+
+/**
+ * Prefect Possition Delete
+ * url - /prefect_possition_delete
+ * method - DELETE
+ * params - pos_name
+ */
+$app->delete('/prefect_possition_delete', 'authenticate', function() use ($app) {
+	
+            // check for required params
+            verifyRequiredParams(array('pos_name'));
+			
+			global $currunt_user_id;
+
+            $response = array();
+
+			// reading post params
+            $pos_name = $app->request->delete('pos_name');
+			
+            $prefectPossitionManagement = new PrefectPossitionManagement();
+			$res = $prefectPossitionManagement->deletePrefectPossition($pos_name, $currunt_user_id);
+			
+            if ($res == DELETE_SUCCESSFULLY) {
+                $response["error"] = false;
+                $response["message"] = "Possition is successfully deleted";
+            } else if ($res == DELETE_FAILED) {
+                $response["error"] = true;
+                $response["message"] = "Oops! An error occurred while deleting possition";
+            } else if ($res == NOT_EXISTED) {
+                $response["error"] = true;
+                $response["message"] = "Sorry, this possition is not exist";
+            }
+            // echo json response
+            echoRespnse(201, $response);
+        });
+
+
+		
+/**
+ * get one prefect possition
+ * method GET
+ * url /prefectPossition/:possitionsName       
+ */
+$app->get('/prefectPossition/:pos_name', 'authenticate', function($pos_name) {
+            global $currunt_user_id;
+            $response = array();
+            
+			$prefectPossitionManagement = new PrefectPossitionManagement();
+			$res = $prefectPossitionManagement->getPossitionByPossitionName($pos_name);
+
+            $response["error"] = false;
+            $response["possition"] = $res;
+
+            
+
+            echoRespnse(200, $response);
+        });
+
+/**
+ * Listing all prefect possitions
+ * method GET
+ * url /prefectPossitions        
+ */
+$app->get('/prefectPossitions', 'authenticate', function() {
+            global $user_id;
+			
+            $response = array();
+			
+            $prefectPossitionManagement = new PrefectPossitionManagement();
+			$res = $prefectPossitionManagement->getAllPossitions();
+
+            $response["error"] = false;
+            $response["possitions"] = array();
+
+            // looping through result and preparing possitions array
+            while ($possitions = $res->fetch_assoc()) {
+                $tmp = array();
+				
+                $tmp["pos_name"] = $possitions["pos_name"];
+                $tmp["status"] = $possitions["status"];
+                $tmp["recode_added_at"] = $possitions["recode_added_at"];
+				$tmp["recode_added_by"] = $possitions["recode_added_by"];
+				
+                array_push($response["possitions"], $tmp);
+            }
+
+            echoRespnse(200, $response);
+        });		
+				
+
+/*
+ * ------------------------ SUPPORTIVE METHODS ------------------------
+ */				
+				
+/**
+ * Verifying required params posted or not
+ */
+function verifyRequiredParams($required_fields) {
+    $error = false;
+    $error_fields = "";
+    $request_params = array();
+    $request_params = $_REQUEST;
+    // Handling PUT request params
+    if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+        $app = \Slim\Slim::getInstance();
+        parse_str($app->request()->getBody(), $request_params);
+    }
+	// Handling PUT request params
+    if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+        $app = \Slim\Slim::getInstance();
+        parse_str($app->request()->getBody(), $request_params);
+    }
+    foreach ($required_fields as $field) {
+        if (!isset($request_params[$field]) || strlen(trim($request_params[$field])) <= 0) {
+            $error = true;
+            $error_fields .= $field . ', ';
+        }
+    }
+
+    if ($error) {
+        // Required field(s) are missing or empty
+        // echo error json and stop the app
+        $response = array();
+        $app = \Slim\Slim::getInstance();
+        $response["error"] = true;
+        $response["message"] = 'Required field(s) ' . substr($error_fields, 0, -2) . ' is missing or empty';
+        echoRespnse(400, $response);
+        $app->stop();
+    }
+}
+
+/**
+ * Echoing json response to client
+ * @param String $status_code Http response code
+ * @param Int $response Json response
+ */
+function echoRespnse($status_code, $response) {
+    $app = \Slim\Slim::getInstance();
+    // Http response code
+    $app->status($status_code);
+
+    // setting response content type to json
+    $app->contentType('application/json');
+
+    echo json_encode($response);
+}
+
+$app->run();
+?>
